ef40925312266b3dc68d747362c18615417527e3
Akshat Behera <62706401+AkshatBehera@users.noreply.github.com>
2021-09-13 20:14:24 +0530

    Experiment 5
    Windows Forms App (.NET Framework) - C# Visual Studio Community 2019

---
+++ PrimeOrNot/PrimeOrNot Program/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimeOrNot_Program
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public Boolean Prime(int n)
+        {
+            for(int i=2; i<=Math.Sqrt(n); i++)
+            {
+                if (n % i==0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(textBox1.Text);
+            if(Prime(x))
+            {
+                textBox2.Text = "Prime Number";
+            }
+            else
+            {
+                textBox2.Text = "Not A Prime Number";
+            }
+        }
+    }
+}
