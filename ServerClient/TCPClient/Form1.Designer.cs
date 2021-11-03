5cefdd94fb7ea6f58c94e5a6b978e428b1a5b08c
Akshat Behera <62706401+AkshatBehera@users.noreply.github.com>
2021-11-04 02:58:11 +0530

    Experiment 6

---
+++ ServerClient/TCPClient/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace TCPClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sendBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputTbx = new System.Windows.Forms.TextBox();
+            this.displayTbx = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // sendBtn
+            // 
+            this.sendBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sendBtn.Location = new System.Drawing.Point(559, 309);
+            this.sendBtn.Name = "sendBtn";
+            this.sendBtn.Size = new System.Drawing.Size(150, 28);
+            this.sendBtn.TabIndex = 0;
+            this.sendBtn.Text = "Send Message";
+            this.sendBtn.UseVisualStyleBackColor = true;
+            this.sendBtn.Click += new System.EventHandler(this.sendBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(50, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Client";
+            // 
+            // inputTbx
+            // 
+            this.inputTbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputTbx.Location = new System.Drawing.Point(54, 309);
+            this.inputTbx.Name = "inputTbx";
+            this.inputTbx.Size = new System.Drawing.Size(456, 24);
+            this.inputTbx.TabIndex = 2;
+            // 
+            // displayTbx
+            // 
+            this.displayTbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayTbx.Location = new System.Drawing.Point(54, 68);
+            this.displayTbx.Multiline = true;
+            this.displayTbx.Name = "displayTbx";
+            this.displayTbx.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.displayTbx.Size = new System.Drawing.Size(655, 219);
+            this.displayTbx.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 396);
+            this.Controls.Add(this.displayTbx);
+            this.Controls.Add(this.inputTbx);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sendBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sendBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputTbx;
+        private System.Windows.Forms.TextBox displayTbx;
+    }
+}
+
