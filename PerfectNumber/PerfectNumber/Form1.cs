32aa193e39e2215de60b28328818c43092fa1446
Akshat Behera <62706401+AkshatBehera@users.noreply.github.com>
2021-11-07 20:51:52 +0530

    Experiment 8

---
+++ PerfectNumber/PerfectNumber/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PerfectNumber
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public Boolean Perfect(int n)
+        {
+            int sum = 0;
+            for (int i=1;i<n;i++)
+            {
+                if(n%i==0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if (n==sum)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(textBox1.Text);
+            if(Perfect(x))
+            {
+                textBox2.Text = "It's Perfect";
+            }
+            else
+            {
+                textBox2.Text = "Not Perfect";
+            }
+        }
+    }
+}
